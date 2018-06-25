cb4f4dbaa6339228a3f6a7f2454504257a96895e
Bill Foote - Cal Poly SLO <wffoote@calpoly.edu>
2018-06-24 22:08:42 -0700

    Put javadocs inside a directory within the zip file

--- build/javadocs.sh
+++ build/javadocs.sh
@@ -3,6 +3,10 @@ cd `dirname $0`/..
 echo "Running in `pwd`"
 rm -rf docs/javadocs
 javadoc -d docs/javadocs -sourcepath src edu.calpoly.spritely
-cd docs/javadocs
-zip -q -r ../../out/spritely-javadocs.zip *
-echo "created out/spritely-javadocs.zip"
+cd docs
+cp -r javadocs spritely-javadocs
+mkdir -p ../out
+rm -f ../out/spritely-javadocs.zip
+zip -q -r ../out/spritely-javadocs.zip spritely-javadocs
+rm -rf spritely-javadocs
+echo "Created out/spritely-javadocs.zip"
