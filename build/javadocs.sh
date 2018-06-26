eac7e0aaa0c34f3d3b5a8567a4003bceb1e75b2e
Bill Foote - Cal Poly SLO <wffoote@calpoly.edu>
2018-06-26 04:57:25 -0700

    Fix damage rect calculation when zoomed. Made build scripts insist on JDK 1.8.

--- build/javadocs.sh
+++ build/javadocs.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+if [ "$JAVA8_HOME" = "" ] ; then
+    echo "Please set the JAVA8_HOME environment variable."
+    exit 1
+fi
 cd `dirname $0`/..
 echo "Running in `pwd`"
 rm -rf docs/javadocs
-javadoc -d docs/javadocs -sourcepath src edu.calpoly.spritely
+$JAVA8_HOME/bin/javadoc -d docs/javadocs -sourcepath src edu.calpoly.spritely
 cd docs
 cp -r javadocs spritely-javadocs
 mkdir -p ../out
--- build/libs.sh
+++ build/libs.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+if [ "$JAVA8_HOME" = "" ] ; then
+    echo "Please set the JAVA8_HOME environment variable."
+    exit 1
+fi
 cd `dirname $0`/..
 rm -rf out/classes
 mkdir -p out/classes
-javac -Xlint:unchecked -Xlint:deprecation -sourcepath src -d out/classes \
+$JAVA8_HOME/bin/javac -Xlint:unchecked -Xlint:deprecation \
+        -sourcepath src -d out/classes \
+	-target 8 -source 8 -bootclasspath $JAVA8_HOME/jre/lib/rt.jar \
 	src/edu/calpoly/spritely/*.java
 if [ $? != 0 ] ; then
     exit 1
